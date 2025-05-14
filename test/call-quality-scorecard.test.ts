f990c391fabbd7f27c2df2e83910db10942c9b47
kb-tan <kobtan@seekasia.com>
2025-05-14 09:23:17 +0800

    Initial commit

---
+++ test/call-quality-scorecard.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CallQualityScorecard from '../lib/call-quality-scorecard-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/call-quality-scorecard-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CallQualityScorecard.CallQualityScorecardStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
